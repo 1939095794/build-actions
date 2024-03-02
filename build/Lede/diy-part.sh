4c2a426f8c7d8af9c1063febaa0ef5bda8bbc171
1939095794 <74325470+1939095794@users.noreply.github.com>
2024-03-03 02:32:59 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -72,6 +72,8 @@ export auto_kernel="true"
 export rootfs_size="2560"
 export kernel_usage="stable"
 
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf package/feeds/packages/v2dat
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
